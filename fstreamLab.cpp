826815cb35e0f58973b996cf68b6b5d9e595ce45
Rostichka <31740019+Rostichka@users.noreply.github.com>
2018-04-14 11:21:56 +0300

    Update fstreamLab.cpp

--- fstreamLab.cpp
+++ fstreamLab.cpp
@@ -10,40 +10,83 @@ int main()
 	char s [3];
 	fin >> s;
 	int n = atoi(s);
-	string line;
+	string ln;
 	getline(fin, line);
 	string *name = new string[n];
 	int *scopes = new int[n];
 	for (int i = 0; i < n; i++) {
-		getline(fin, line);
+		getline(fin, ln);
 		int j = 0;
 		int scope = 0;
-		while (line[j] != ',')
+		while (ln[j] != ',')
 		{
-			name[i] += line[j];
+			name[i] += ln[j];
 			j++;
 		}
 		j++;
-		while (j < line.length()) {
-			if (line[j] > line[j + 2]) scope += 3; 
-			else if (line[j] == line[j + 2]) scope += 1;
+		while (j < ln.length()) {
+			if (ln[j] > ln[j + 2]) score += 3;
+			else if (ln[j] == ln[j + 2]) score += 1;
 			j += 4;
 		}
-		scopes[i] = scope;
+		scores[i] = score;
 	}
 	for (int k = 0; k < n; k++)
 	{
-		int scope = 0, j;
+		int score = 0, j;
 		for (int i = 0; i < n; i++)
 		{
-			if (scopes[i] > scope)
+			if (scores[i] > score)
 			{
-				scope = scopes[i];
+				score = scores[i];
 				j = i;
 			}
 		}
-		fout << k + 1 << ". " << name[j] << ": " << scope << '.'<< endl;
-		scopes[j] = -1;
+		fout << k + 1 << ". " << name[j] << ": " << score << '.'<< endl;
+		scores[j] = -1;
+	}
+	return 0;
+}
+
+	ifstream fin("premier_league.csv");
+	ofstream fout("result.csv");
+	char s [3];
+	fin >> s;
+	int n = atoi(s);
+	string ln;
+	getline(fin, ln);
+	string *name = new string[n];
+	int *scores = new int[n];
+	for (int i = 0; i < n; i++) {
+		getline(fin, ln);
+		int j = 0;
+		int score = 0;
+		while (ln[j] != ',')
+		{
+			name[i] += ln[j];
+			j++;
+		}
+		j++;
+		while (j < ln.length()) {
+			if (ln[j] > ln[j + 2]) score += 3; 
+			else if (ln[j] == ln[j + 2]) score += 1;
+			j += 4;
+		}
+		scores[i] = score;
+	}
+	for (int k = 0; k < n; k++)
+	{
+		int score = 0, j;
+		for (int i = 0; i < n; i++)
+		{
+			if (scores[i] > score)
+			{
+				score = scores[i];
+				j = i;
+			}
+		}
+		fout << k + 1 << "." << name[j] << ":" << score << '.'<< endl;
+		scores[j] = -1;
 	}
 	return 0;
 }
